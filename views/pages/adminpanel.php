73913dfa0bc3906aea4da18679793ec8c05e63d0
voladosas.05@mail.ru <voladosas.05@mail.ru>
2023-03-03 16:28:13 +0300

    добавил файл admin

---
+++ views/pages/adminpanel.php
@@ -0,0 +1,29 @@
+<?php
+
+use app\Services\Page;
+use app\Views\Views;
+
+?>
+<!doctype html>
+<html lang="en">
+<?php Page::part("header"); ?>
+<body>
+<?php Page::part("nav"); ?>
+<div class="container">
+<div class="line"></div>
+    <?php endif; ?>
+    
+        <p class="aft">Почему ты еще не авторизован!</p>
+        <div class="wrapper">
+            <header class="header">Я фывф</header>  
+            <main class="main">Я фывф</main> 
+            <footer class="footer">Я фывф</footer> 
+        </div>
+        
+
+
+<!--<script src="../../assets/js/ajax.js"></script>-->
+
+
+</body>
+</html>
